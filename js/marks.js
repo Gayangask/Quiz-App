780c3613b95ce2820abd77c1ffa297d77fb3cdeb
Gayanga <gayangask@gmail.com>
2026-01-10 07:50:59 +0530

    Changes Commited

--- js/marks.js
+++ js/marks.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const score = localStorage.getItem("finalScore");
     const marks = document.getElementById("marks");
 
-    marks.innerText = score;
+    marks.innerText = ": " + score + " %";
 
 });
