43e8387175b5350a02c7b2503413109d3e2cf51b
Gayangask <gayangask@gmail.com>
2026-01-10 07:41:05 +0530

    Add files via upload

---
+++ js/marks.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const score = localStorage.getItem("finalScore");
+    const marks = document.getElementById("marks");
+
+    marks.innerText = score;
+
+});
